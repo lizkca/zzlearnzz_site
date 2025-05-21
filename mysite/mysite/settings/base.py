ee4f04e58d19222d8690bd4c2d88b17ace9ed6ae
lizkca <lienxiong@outlook.com>
2025-05-21 20:10:09 +0800

    production.py

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -12,24 +12,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vsnnxjh8ka3cfcau+_((p(&kx=x@q(ch(pl%0e)z_g146ohws6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+# 共享的基础设置
+SECRET_KEY = 'your-secret-key'
+DEBUG = False
 ALLOWED_HOSTS = []
 
-
-# Application definition
-
+# 应用列表
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
